858a81e56ae3587049d4ba7875cac52719e2780f
Mila Kalugina <milkalugin@gmail.com>
2020-05-19 03:26:13 +0300

    добавила анимацию в в последний блок

---
+++ js/sketch_core.js
@@ -0,0 +1,30 @@
+let img;
+function preload() {
+  books = loadImage('../imgs/книги.png');
+  cake = loadImage('../imgs/тортик.png');
+}
+
+function setup() {
+    // Создаем канвас, как весь браузер
+    let myCanvas = createCanvas(windowWidth, windowHeight);
+    // прикрепляем канвас в заранее созданный элемент
+    myCanvas.parent("canvas");
+    background('#FF7B7B');
+}
+
+function draw() {
+    noStroke();
+    rect('#FF7B7B', 0, 0, windowWidth, windowHeight);
+
+    stroke(0);
+    if (mouseIsPressed) {
+        image(books, mouseX, mouseY, 100, 100);
+    } else {
+        image(cake, mouseX, mouseY, 100, 100);
+    }
+}
+
+// Изменяем размер канваса, если изменился размер окна
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
