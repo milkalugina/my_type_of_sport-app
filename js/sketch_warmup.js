1034d16d8685dcaaa94030d87fc7a303da750450
Mila Kalugina <milkalugin@gmail.com>
2020-05-19 10:15:19 +0300

    исправила ошибки в тексте

---
+++ js/sketch_warmup.js
@@ -0,0 +1,29 @@
+function preload() {
+  stars = loadImage('../imgs/звездочки.png');
+  go = loadImage('../imgs/гоу.png');
+}
+
+function setup() {
+    // Создаем канвас, как весь браузер
+    let myCanvas = createCanvas(windowWidth, windowHeight);
+    // прикрепляем канвас в заранее созданный элемент
+    myCanvas.parent("canvas");
+    background('#FF7B7B');
+}
+
+function draw() {
+    noStroke();
+    rect('#FF7B7B', 0, 0, windowWidth, windowHeight);
+
+    stroke(0);
+    if (mouseIsPressed) {
+        image(go, mouseX, mouseY, 150, 150);
+    } else {
+        image(stars, mouseX, mouseY, 250, 250);
+    }
+}
+
+// Изменяем размер канваса, если изменился размер окна
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
